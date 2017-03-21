41ee9c4228fa8b4ba0deec5fa8a918b5928a1f14
DESKTOP-KP0GSC5\heyunchao <yunchaohe@gmail.com>
2017-03-21 17:28:06 +0800

    archive to a .a lib

---
+++ start_compile_lib.sh
@@ -0,0 +1,15 @@
+# -I ./SRC/
+# g++ -lpthread -lrt -lcurl -funsigned-char SRC/*.cpp -O2 -oLinuxChatScript 2>err.txt
+# g++ -funsigned-char SRC/*.cpp -O2 -DDISCARDDATABASE=1 -o LinuxChatScript -lpthread -lrt -lcurl 2>>err.txt
+
+
+# Dependencies: 
+# 1. libcurl (http://blog.csdn.net/qianghaohao/article/details/51684862)
+
+cd ChatScript-7.3/SRC/
+
+make clean
+echo "Start Compiling"
+
+make cs -f Makefile_lib
+echo "Compile End"
