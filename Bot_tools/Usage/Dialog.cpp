869188204f638f219c03b0b4943e97c508e0d2f6
DESKTOP-KP0GSC5\heyunchao <yunchaohe@gmail.com>
2017-04-05 11:53:37 +0800

    chatloop

--- Bot_tools/Usage/Dialog.cpp
+++ Bot_tools/Usage/Dialog.cpp
@@ -1,6 +1,42 @@
 #include "common.h" 
 #include "evserver.h"
 
+// char sourceInput_1[200];
+
+void ChatLoop() //   local machine loop
+{
+	
+	char user[MAX_WORD_SIZE];
+	*ourMainInputBuffer = 0;
+	sourceFile = stdin;
+	// if (*sourceInput_1)	sourceFile = FopenReadNormal(sourceInput_1);
+	// else 
+	if (userInitFile) sourceFile = userInitFile;
+	if (!*loginID)
+	{
+		printf((char*)"%s", (char*)"\r\nEnter user name: ");
+		ReadALine(user, stdin);
+		printf((char*)"%s", (char*)"\r\n");
+		if (*user == '*') // let human go first  -   say "*bruce
+		{
+			memmove(user, user + 1, strlen(user));
+			printf((char*)"%s", (char*)"\r\nEnter starting input: ");
+			ReadALine(ourMainInputBuffer, stdin);
+			printf((char*)"%s", (char*)"\r\n");
+		}
+	}
+	else strcpy(user, loginID);
+	PerformChat(user, computerID, ourMainInputBuffer, NULL, ourMainOutputBuffer); // unknown bot, no input,no ip
+	printf("%s %s", "output: ", ourMainOutputBuffer);
+
+retry:
+	ProcessInputFile();
+	sourceFile = stdin;
+	*ourMainInputBuffer = 0;
+	ourMainInputBuffer[1] = 0;
+	if (!quitting) goto retry;
+}
+
 int main(int argc, char * argv[])
 {
 	for (int i = 1; i < argc; ++i)
@@ -9,7 +45,7 @@ int main(int argc, char * argv[])
 		{
 			chdir((char*)argv[i] + 5);
 		}
-		}
+	}
 
 	FILE* in = FopenStaticReadOnly((char*)"SRC/dictionarySystem.h"); // SRC/dictionarySystem.h
 	if (!in) // if we are not at top level, try going up a level
@@ -21,7 +57,8 @@ int main(int argc, char * argv[])
 	if (!server)
 	{
 		quitting = false; // allow local bots to continue regardless
-		MainLoop();
+		// MainLoop();
+		ChatLoop();
 	}
 	else if (quitting) { ; } // boot load requests quit
 	CloseSystem();
